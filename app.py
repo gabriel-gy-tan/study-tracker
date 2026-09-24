402379a8e2f8d7be7b6c2f37d95a4039a346c0c2
Gabriel Tan <gabrielgytan@gmail.com>
2026-09-24 14:23:34 +0400

    Added functionality for the login and the register! Works now so onto the next part

--- app.py
+++ app.py
@@ -25,11 +25,42 @@ app.secret_key = os.getenv("SECRET_KEY")
 def index():
     return render_template("index.html")
 
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        
+        username = request.form.get("username")
+        password = request.form.get("password")
+        confirmation = request.form.get("confirmation")
+        if not username or not password:
+            flash("Enter a username and password")
+            return redirect("/register")
+        
+        connection = get_db_connection()
+        check = connection.execute("SELECT * FROM users WHERE username = ?", (username,)).fetchall()
+
+        if len(check) > 0 :
+            connection.close()
+            flash("Please choose a different username")
+            return redirect("/register")
+
+        if password != confirmation:
+            flash("Please enter the same password twice")
+            return redirect("/register")
+
+        connection.execute("INSERT INTO users (username, hash) VALUES(?, ?)", (username, generate_password_hash(password)))
+        connection.commit()
+        connection.close()
+
+        return render_template("login.html")
+
+    else:
+        return render_template("register.html")
+    
 @app.route("/login", methods=["GET", "POST"])
 def login():
     
     if request.method == "POST":
-        print("post received")
         username = request.form.get("username")
         if not username:
             flash("Please enter a username")
@@ -49,6 +80,18 @@ def login():
             return redirect("/login")
 
         session["user_id"] = user[0]["id"]
+        print("Logged in")
+        return redirect("/")
 
     
     return render_template("login.html")
+
+#Took this one straight from cs50
+@app.route("/logout")
+def logout():
+
+    # Forget any user_id
+    session.clear()
+
+    # Redirect user to login form
+    return redirect("/")
